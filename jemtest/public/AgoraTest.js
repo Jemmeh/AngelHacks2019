219c0d056387c20ff3869dcf8daaedb000c35c6a
jcope <thejemmeh@gmail.com>
2019-06-29 15:25:48 -0700

    remove jemtest

--- jemtest/public/AgoraTest.js
+++
@@ -1,8 +0,0 @@
-var client = AgoraRTC.createClient({mode: 'live', codec: "h264"});
-
-client.init('069f570d02484c6d9a7ad12e55091268', function () {
-    console.log("AgoraRTC client initialized");
-  
-  }, function (err) {
-    console.log("AgoraRTC client init failed", err);
-  });

dffab0037afdb936975f4015ff426c111f376396
Hafzul Bhuiyan <havi@Hafzuls-MacBook-Pro.local>
2019-06-30 06:14:32 -0700

    Added Facial Rekognition

--- angelhackfe/src/components/video/index.js
+++ angelhackfe/src/components/video/index.js
@@ -1,38 +1,169 @@
 import React from "react";
- 
+import Webcam from "react-webcam";
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  region: "us-east-1",
+  accessKeyId: "AKIASDJ7LHTVDQBR6YGV",
+  secretAccessKey: "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"
+});
+
+const rekog = new AWS.Rekognition();
+
 class Video extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {videoSrc: null}
+    this.state = {
+      videoSrc: null,
+      images: {
+        img1: null,
+        img2: null
+      },
+      imgConfidence: 0,
+      params: {}
+    };
   }
 
-  componentDidMount(){
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+  componentDidMount() {
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia ||
+      navigator.oGetUserMedia;
     if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: true, audio: true}, this.handleVideo, this.videoError);
+      navigator.getUserMedia(
+        { video: true, audio: true },
+        this.handleVideo,
+        this.videoError
+      );
     }
   }
 
   takePicture = () => {
     let canvas = this.refs.canvas;
-    canvas.getContext("2d").drawImage(this.refs.video, 0, 0, 300, 300, 0, 0, 300, 300);
-    canvas.toDataURL("image/png");
+    canvas
+      .getContext("2d")
+      .drawImage(this.refs.video, 0, 0, 300, 300, 0, 0, 300, 300);
+    this.validateImg(canvas.toDataURL("image/jpeg"));
   };
-  
+
+  validateImg = image => {
+    let imgs = this.state.images;
+    var base64Image = image.replace(/^data:image\/(png|jpeg|jpg);base64,/, "");
+    var imageBytes = this.getBinary(base64Image);
+    let params = {
+      Image: {
+        Bytes: imageBytes
+      },
+      Attributes: ["ALL"]
+    };
+    var valid = 0;
+    rekog.detectFaces(params, (err, data) => {
+      if (err) console.log(err);
+      else {
+        console.log(data);
+        if (data.FaceDetails[0] != undefined) {
+          console.log(
+            `this is the confidence: ${data.FaceDetails[0].Confidence} `
+          );
+          let confidence = data.FaceDetails[0].Confidence;
+          let num = 80;
+          valid = confidence > num ? 1 : 0;
+          console.log("val :", valid);
+          if (valid) {
+            imgs.img1 === null
+              ? (imgs.img1 = imageBytes)
+              : (imgs.img2 = imageBytes);
+            console.log(imgs.img1, "and ", imgs.img2);
+            this.setState({ images: imgs });
+            if (imgs.img2) {
+              this.validateFaces();
+            } else {
+              alert("Take Picture of Valid ID");
+            }
+          } else {
+            console.log("not valid");
+            alert("Unable to detect face!");
+          }
+        } else {
+          console.log("Unable to detect face!");
+          alert("Unable to detect face!");
+          this.setState({
+            images: {
+              img1: null,
+              img2: null
+            }
+          });
+        }
+      }
+    });
+  };
+
+  getBinary = base64Image => {
+    var binaryImg = atob(base64Image);
+    var length = binaryImg.length;
+    var ab = new ArrayBuffer(length);
+    var ua = new Uint8Array(ab);
+    for (var i = 0; i < length; i++) {
+      ua[i] = binaryImg.charCodeAt(i);
+    }
+
+    return ab;
+  };
+
+  validateFaces = () => {
+    let images = this.state.images;
+    let params = {
+      SimilarityThreshold: 85,
+      SourceImage: {
+        Bytes: images.img1
+      },
+      TargetImage: {
+        Bytes: images.img2
+      }
+    };
+    rekog.compareFaces(params, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (data.FaceMatches[0] != undefined) {
+          console.log(data);
+          let similarity = data.FaceMatches[0].Similarity;
+          let num = 90;
+          if (similarity >= num) {
+            console.log("Match");
+          } else {
+            console.log("Not A Match!");
+          }
+        } else {
+          console.log("Not A Match!");
+          alert("Not A Match!");
+          this.setState({
+            images: {
+              img1: null,
+              img2: null
+            }
+          });
+        }
+      }
+    });
+  };
+
   handleVideo = stream => {
     this.refs.video.srcObject = stream;
   };
 
-  videoError = () => {
-
-  };
+  videoError = () => {};
 
   render() {
     return (
       <div>
-        <video autoPlay={true} ref="video"/>
-        <button type="button" onClick={this.takePicture}>Take Picture</button>
-	      <canvas width="300" height="300" ref="canvas"></canvas>
+        <video autoPlay={true} ref="video" />
+        <button type="button" onClick={this.takePicture}>
+          Take Picture
+        </button>
+        <canvas width="300" height="300" ref="canvas" />
       </div>
     );
   }
